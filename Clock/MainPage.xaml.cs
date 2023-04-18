982f40d6e6320e85a746e62ff1ea7ebcaf750940
SADXBVSTVRD <SADXBVSTVRD@DESKTOP-1LOL4LG>
2023-04-18 07:56:59 +0500

    Добавьте файлы проекта.

---
+++ Clock/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿namespace Clock;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+
+    //private void OnCounterClicked(object sender, EventArgs e)
+    //{
+    //	count++;
+
+    //	if (count == 1)
+    //		CounterBtn.Text = $"Clicked {count} time";
+    //	else
+    //		CounterBtn.Text = $"Clicked {count} times";
+
+    //	SemanticScreenReader.Announce(CounterBtn.Text);
+    //}
+
+    //private async void TapGestureRecognizer_Tapped(object sender, TappedEventArgs e)
+    //{
+    //    if (sender is BoxView myBoxView)
+    //    {
+    //        //myBoxView.WidthRequest += 10;
+    //        await myBoxView.RelRotateTo(90, 300);
+    //    }
+    //}
+}
+
